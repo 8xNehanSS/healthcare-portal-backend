cf9e9234f350e2f487724054b2ce29aa94ff7827
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-07-06 18:33:03 +0530

    user controller full implementation

--- controllers/userController.go
+++ controllers/userController.go
@@ -13,9 +13,12 @@ func CreateUser (c *gin.Context) {
 		FirstName string `json:"first_name"`
 		LastName string `json:"last_name"`
 		Address string `json:"address"`
-		Email *string `json:"email"`
+		Email string `json:"email"`
 		Age uint8 `json:"age"`
 		Birthday *time.Time `json:"birthday"`
+		Username string `json:"username"`
+		Password string `json:"password"`
+		Type uint `json:"type"`
 	}
 
 	c.Bind(&body)
@@ -27,6 +30,11 @@ func CreateUser (c *gin.Context) {
 		Email: body.Email,
 		Age: body.Age,
 		Birthday: body.Birthday,
+	}
+	err := CreateLogin(body.Username, body.Email, body.Password, body.Type, user.ID, user)
+	if err != nil {
+		c.Status(400)
+		return
 	}
 	result := initializers.DB.Create(&user)
 	if result.Error != nil {
@@ -44,4 +52,53 @@ func GetUsers (c *gin.Context) {
 	c.JSON(200, gin.H{
 		"users": users,
 	})
+}
+
+func GetUser (c *gin.Context) {
+	id := c.Param("id")
+	var user models.User
+	initializers.DB.First(&user, id)
+	c.JSON(200, gin.H{
+		"user": user,
+	})
+}
+
+func UserUpdate (c *gin.Context) {
+	id := c.Param("id")
+	var body struct {
+		FirstName string `json:"first_name"`
+		LastName string `json:"last_name"`
+		Address string `json:"address"`
+		Email string `json:"email"`
+		Age uint8 `json:"age"`
+		Birthday *time.Time `json:"birthday"`
+	}
+
+	c.Bind(&body)
+
+	var user models.User
+	initializers.DB.First(&user, id)
+
+	userUpdated := models.User{
+		FirstName: body.FirstName,
+		LastName: body.LastName,
+		Address: body.Address,
+		Email: body.Email,
+		Age: body.Age,
+		Birthday: body.Birthday,
+	}
+
+	initializers.DB.Model(&user).Updates(userUpdated)
+
+	c.JSON(200, gin.H{
+		"user": user,
+	})
+}
+
+func UserDelete (c *gin.Context) {
+	id := c.Param("id")
+	var user models.User
+	initializers.DB.First(&user, id)
+	initializers.DB.Delete(&user)
+	c.Status(200)
 }
