fd710ece7d688f8e9f53c8c5f9dcabfd70f8011a
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-07-06 18:32:53 +0530

    main routes added

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"healthcare-portal/controllers"
 	"healthcare-portal/initializers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,7 +15,17 @@ func init() {
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.Default())
+
+	// entry points
 	r.POST("/register", controllers.CreateUser)
+	r.POST("/login", controllers.CheckLogin)
+
+	// users
 	r.GET("/users", controllers.GetUsers)
+	r.GET("/users/:id", controllers.GetUser)
+	r.PUT("/users/:id", controllers.UserUpdate)
+	r.DELETE("/users/:id", controllers.UserDelete)
+
 	r.Run()
 }
