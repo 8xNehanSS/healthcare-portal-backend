8195083a76f55bb9c1293016635884789548bef6
8xNehanSS <nehan.sudasinghe@gmail.com>
2024-07-22 17:51:00 +0530

    completed create appointment endpoint

---
+++ controllers/patientController.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"fmt"
+	"healthcare-portal/initializers"
+	"healthcare-portal/models"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateAppointment(c *gin.Context) {
+	var body struct {
+		PatientID uint      `json:"patient_id"`
+		DoctorID  uint      `json:"doctor_id"`
+		Date      time.Time `json:"date"`
+		Reason    string    `json:"reason"`
+	}
+
+	c.Bind(&body)
+
+	appointment := models.Appointment{
+		PatientID: body.PatientID,
+		DoctorID:  body.DoctorID,
+		Date:     time.Now(),
+		Reason:    body.Reason,
+	}
+
+	result := initializers.DB.Create(&appointment)
+	if result.Error != nil {
+		fmt.Println(result.Error)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Appointment creation failed",
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "Appointment created successfully",
+	})
+
+}
